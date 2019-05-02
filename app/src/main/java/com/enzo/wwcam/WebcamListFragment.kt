58f9d8fee07b547fb2b44fc3e0b828bc483061fa
Nikita Kazantsev <enzomatrix89@gmail.com>
2019-05-01 22:18:27 -0500

    Added app's context menu

--- app/src/main/java/com/enzo/wwcam/WebcamListFragment.kt
+++ app/src/main/java/com/enzo/wwcam/WebcamListFragment.kt
@@ -2,9 +2,7 @@ package com.enzo.wwcam
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
@@ -54,6 +52,8 @@ class WebcamListFragment : Fragment() {
             webcamAdapter.notifyDataSetChanged()
             progressBar.visibility = View.GONE
         })
+
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -70,27 +70,24 @@ class WebcamListFragment : Fragment() {
         progressBar.visibility = View.VISIBLE
 
         val recyclerView = view.findViewById(R.id.webcam_recyclerview) as RecyclerView
-
-//        linearLayoutManager = LinearLayoutManager(activity)
-//        recyclerView.layoutManager = linearLayoutManager
-//        recyclerView.adapter = webcamAdapter
-
         gridLayoutManager = GridLayoutManager(activity, 2)
         recyclerView.layoutManager = gridLayoutManager
         recyclerView.adapter = webcamAdapter
 
-        val settings = view.findViewById<Button>(R.id.settings_button)
-        settings.setOnClickListener{
+        return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.searchMenuItem) {
             val intent = Intent(activity, WebcamListParamsActivity::class.java)
             startActivityForResult(intent, WEBCAM_LIST_PARAMS)
+            return true
         }
-
-
-
-//        val webcamCardView = view.findViewById(R.id.webcamCardView) as CardView
-
-
-        return view
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
